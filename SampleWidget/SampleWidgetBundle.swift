b3dc03b49b5ea28f0dce21d30355d4f13ebe2a87
Miquel Bosch <miquel.bosch.cortes@playsportsnetwork.com>
2023-07-03 17:25:15 +0200

    add sample widget

---
+++ SampleWidget/SampleWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SampleWidgetBundle.swift
+//  SampleWidget
+//
+//  Created by Miguel Bosch Cortés on 03/07/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SampleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SampleWidget()
+    }
+}
